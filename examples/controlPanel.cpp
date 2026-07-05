300efc7cde67212f160ac7e785aecef0a3547edd
Muhammad Hasan <mhd.hasan236@gmail.com>
2026-07-05 02:06:01 +0000

    Added vertical navigation and value editing to the dashboard

--- examples/controlPanel.cpp
+++ examples/controlPanel.cpp
@@ -11,9 +11,10 @@ enum class Panel {
 };
 
 struct RunState {
-    ControlType mode = DUTY_CYCLE;
+    int mode = 0;
     float setpoint = 0.0f;
     uint8_t slot = 0;
+    int active_field = 0;
 };
 
 struct PidfState {
@@ -66,6 +67,48 @@ int main(int argc, char* argv[]) {
             case KEY_LEFT:
                 ui.active_panel = (ui.active_panel == Panel::Run) ? Panel::Pidf : Panel::Run;
                 break;
+
+            case KEY_UP:
+                if (ui.active_panel == Panel::Run) {
+                    run.active_field = (run.active_field + 2) % 3;
+                }
+                else {
+                    pidf.active_field = (pidf.active_field + 3) % 4;
+                } break;
+            case KEY_DOWN:
+                if (ui.active_panel == Panel::Run) {
+                    run.active_field = (run.active_field + 1) % 3;
+                }
+                else {
+                    pidf.active_field = (pidf.active_field + 1) % 4;
+                } break;
+
+            case '+':
+            case '=':
+                if (ui.active_panel == Panel::Run) {
+                    if (run.active_field == 0) run.mode = (run.mode + 2) % 3;
+                    else if (run.active_field == 1) run.setpoint +=0.05f;
+                    else if (run.active_field == 2 && run.slot > 0) run.slot += 1;
+                } else {
+                    if (pidf.active_field == 0) pidf.p += 0.001f;
+                    else if (pidf.active_field == 1) pidf.i += 0.001f;
+                    else if (pidf.active_field == 2) pidf.d += 0.001f;
+                    else if (pidf.active_field == 3) pidf.d += 0.001f;
+                } break;
+
+            case '-':
+            case '_':
+                if (ui.active_panel == Panel::Run) {
+                    if (run.active_field == 0) run.mode = (run.mode + 2) % 3;
+                    else if (run.active_field == 1) run.setpoint -= 0.05f;
+                    else if (run.active_field == 2 && run.slot > 0) run.slot -= 1;
+                } else {
+                    if (pidf.active_field == 0) pidf.p -= 0.001f;
+                    else if (pidf.active_field == 1) pidf.i -= 0.001f;
+                    else if (pidf.active_field == 2) pidf.d -= 0.001f;
+                    else if (pidf.active_field == 3) pidf.f -= 0.001f;
+                } break;
+            
         }
 
         werase(stdscr); 
@@ -78,22 +121,45 @@ int main(int argc, char* argv[]) {
         mvprintw(0, 2, " SparkMAX control panel ");
         mvprintw(2, 2, "CAN ID: %d | Press 'q' to quit | Left/Right arrows change tabs", ui.current_can_id);
 
-        if (ui.active_panel == Panel::Run) attron(A_REVERSE); 
+        bool run_focus = (ui.active_panel == Panel::Run);
+
+        if (run_focus) attron(A_REVERSE); 
         mvprintw(5, 2, " - Run panel - ");
-        if (ui.active_panel == Panel::Run) attroff(A_REVERSE);
-        
+        if (run_focus) attroff(A_REVERSE);
+
+        if (run_focus && run.active_field == 0) attron(A_REVERSE);
         mvprintw(7, 4, "Control Mode: %d", run.mode);
+        if (run_focus && run.active_field == 0) attroff(A_REVERSE);
+
+        if (run_focus && run.active_field == 1) attron(A_REVERSE);
         mvprintw(8, 4, "Setpoint:     %.2f", run.setpoint);
+        if (run_focus && run.active_field == 1) attroff(A_REVERSE);
+
+        if (run_focus && run.active_field == 2) attron(A_REVERSE);
         mvprintw(9, 4, "PID Slot:     %d", run.slot);
+        if (run_focus && run.active_field == 2) attroff(A_REVERSE);
 
-        if (ui.active_panel == Panel::Pidf) attron(A_REVERSE); 
+        bool pidf_focus = (ui.active_panel == Panel::Pidf);
+
+        if (pidf_focus) attron(A_REVERSE); 
         mvprintw(5, mid_x + 2, " - PIDF tuning - ");
-        if (ui.active_panel == Panel::Pidf) attroff(A_REVERSE);
+        if (pidf_focus) attroff(A_REVERSE);
 
+        if (pidf_focus && pidf.active_field == 0) attron(A_REVERSE);
         mvprintw(7, mid_x + 4, "P: %.4f", pidf.p);
+        if (pidf_focus && pidf.active_field == 0) attroff(A_REVERSE);
+
+        if (pidf_focus && pidf.active_field == 1) attron(A_REVERSE);
         mvprintw(8, mid_x + 4, "I: %.4f", pidf.i);
+        if (pidf_focus && pidf.active_field == 1) attroff(A_REVERSE);
+
+        if (pidf_focus && pidf.active_field == 2) attron(A_REVERSE);
         mvprintw(9, mid_x + 4, "D: %.4f", pidf.d);
+        if (pidf_focus && pidf.active_field == 2) attroff(A_REVERSE);
+
+        if (pidf_focus && pidf.active_field == 3) attron(A_REVERSE);
         mvprintw(10, mid_x + 4, "F: %.4f", pidf.f);
+        if (pidf_focus && pidf.active_field == 3) attroff(A_REVERSE);
 
         mvprintw(mid_y + 1, 2, " - Live feedback - ");
         mvprintw(mid_y + 3, 4, "Position: 0.000");
@@ -104,8 +170,8 @@ int main(int argc, char* argv[]) {
         refresh();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    
     }
-
     endwin();
     return 0;
 }
